cc4e870dc52b9ba183a05ad19fb6b55f2eb5198c
raja1995 <rajaece154@gmail.com>
2016-09-19 01:26:14 +0530

    Update vowels.java

--- vowels.java
+++ vowels.java
@@ -1,1 +1,21 @@
 import.java.util.*;
+public class java
+{
+  public static void main(String[] args)
+  {
+    Scanner in=new scanner(System.in);
+    String s=in.next();
+    int vowels=0;
+    for(int i=0;i<s.length();i++)
+    {
+    if(s.charAt(i)=='A'||s.charAt(i)=='a'||s.charAt(i)=='E'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='I'||s.charAt(i)=='o'||s.charAt(i)=='O'||s.charAt(i)=='u'||s.charAt(i)=='U');
+    {
+    vowels++;
+    }
+    }
+    System.out.print(vowels);
+  }
+}
+
+
+
