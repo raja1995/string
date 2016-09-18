33dac243bc673adaa01086fb1f0a4446f28943af
raja1995 <rajaece154@gmail.com>
2016-09-19 00:31:14 +0530

    Update even.java

--- even.java
+++ even.java
@@ -1,2 +1,26 @@
 import.Scanner.util*
-public class odd{
+public class odd
+{
+  public static void main(String[] args)
+  {
+    Scanner in=new Scanner(System.in);
+    String s=in.next();
+    String n="";
+    for(int i=0;i<s.length;i++)
+    {
+      if(i%2==0)
+      {
+        n=n+s.charAt(i+1);
+      }
+      else
+      {
+        n=n+s.charAt(i-1);
+      }
+    }
+    System.out.print(n);
+  }
+  
+}
+
+
+
