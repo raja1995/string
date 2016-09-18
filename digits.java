41449fdea55f567c759d5af0b6e2143dd96a3607
raja1995 <rajaece154@gmail.com>
2016-09-19 01:43:30 +0530

    Update digits.java

--- digits.java
+++ digits.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+public class java
+{
+  public static void main(string[] args)
+  {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int count=0;
+    String s=String.valueOf(n);
+    for(int i=1;i<s.length();i+2)
+    {
+      count=count+(int)(s.charAt(i));
+    }
+    System.out.print(count);
+  }
+}
