ea4d2625e4d2dbcc210f534b87c9ae72a91e54fb
raja1995 <rajaece154@gmail.com>
2016-09-19 01:12:25 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,2 +1,26 @@
 import.java.util.*
-p
+public class even
+{
+  public static void main(String[] args)
+  {
+    Scanner in=new Scanner(System.in);
+    String s=in.next();
+    int count=0,max=0;
+    char[] ch=s.tocharArray();
+    for(int i=0;i<ch.length;i++)
+    {
+      for(int j=i+1;j<ch.length;j++)
+      {
+       if(ch[i]==ch[j])
+        {
+         count++;
+        }
+      }
+      max=ch.length-count;
+      System.out.print(max);
+    }
+  }
+}
+
+
+
