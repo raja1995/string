9af36dac444683c9b01f6cd692f10f5530d4b10d
raja1995 <rajaece154@gmail.com>
2016-09-19 00:18:14 +0530

    Update stringsa.java

--- stringsa.java
+++ stringsa.java
@@ -1,2 +1,11 @@
+import java.util.*;
 public class array{
+    public static void main(String[] args)
+  {
+    Scanner in=new Scanner(System.in);
+    String s=in.next();
+    int n=Integer.valueOf(s);
+    System.out.print(n);
+  }
+}
 
