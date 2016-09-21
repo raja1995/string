4a402fe78f39c7f71c6e882306b1c05e36e82b8c
raja1995 <rajaece154@gmail.com>
2016-09-21 23:42:39 +0530

    Update net.java

--- net.java
+++ net.java
@@ -7,12 +7,13 @@ public class java
     String s=in.next();
     String s2="";
     String s1=new StringBuffer(s).toreverse().toString();
+    System.out.println(s1);
     for(int i=0;i<s1.length();i++){
       if(s1.charAt(i)!='a'||s1.charAt(i)!='e'||s1.charAt(i)!='i'||s1.charAt(i)!='o'||s1.charAt(i)!='u'||s1.charAt(i)!='A'||s1.charAt(i)!='E'||s1.charAt(i)!='I'||s1.charAt(i)!='O'||s1.charAt(i)!='U')
       {
         s2=s2+s1.charAt(i);
       }
     }
-  System.out.print(s2);
+  System.out.println(s2);
   }
 }
